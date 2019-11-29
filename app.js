b8c8a20828742b6c6f52e6aae17711b1e27dd602
Saurabh Sharma <saurabh.sharma@mail.vinove.com>
2019-11-29 10:37:24 +0530

    UPDATED | added jumbotron function in app.js

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ function printLine()
 {
     return `Speak of the devil!`;
 }
+function printJumboTronLine()
+{
+    return `Speak of the devil, and Devil is here !`;
+}
 
 /**
  * A Heading  function
  * @returns {string}
  */
+<<<<<<< HEAD
 function addHeading()
 {
     return `Don't count your chickens before the eggs have hatched`;
+}
+function addJumboTron()
+{
+    return `Don't put all your eggs in one basket. Don't count your chickens before the eggs have hatched`;
 }
